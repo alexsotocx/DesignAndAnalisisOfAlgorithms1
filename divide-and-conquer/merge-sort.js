6205de9f074d2e126f3a501317c5e7c9efbb92c5
Alexander Soto Cardona <asoto@innventto.com>
2015-07-01 08:08:34 -0500

    Transalte tabs to spaces

--- divide-and-conquer/merge-sort.js
+++ divide-and-conquer/merge-sort.js
@@ -1,33 +1,32 @@
-
 var Sort = {
-	mergeSort: function(inputArray) {
-		var N = inputArray.length
-		var tmpArray = new Array(N)
-		
-		function merge(lo, mid, hi) {
-			var i = lo, j = mid + 1;
-			var k = 0;
-			while(i <= mid || j <= hi){
-				if( (i <= mid && inputArray[i] <= inputArray[j]) || j > hi)
-					tmpArray[k++] = inputArray[i++]
-				else
-					tmpArray[k++] = inputArray[j++]
-			}
+  mergeSort: function(inputArray) {
+    var N = inputArray.length
+    var tmpArray = new Array(N)
+    
+    function merge(lo, mid, hi) {
+      var i = lo, j = mid + 1;
+      var k = 0;
+      while(i <= mid || j <= hi){
+        if( (i <= mid && inputArray[i] <= inputArray[j]) || j > hi)
+          tmpArray[k++] = inputArray[i++]
+        else
+          tmpArray[k++] = inputArray[j++]
+      }
 
-			for(i = 0; i < k; i++)
-				inputArray[lo++] = tmpArray[i]
-		}
+      for(i = 0; i < k; i++)
+        inputArray[lo++] = tmpArray[i]
+    }
 
-		function solve(lo, hi) {
-			if(hi <= lo) return;
-			var mid = Math.floor((hi + lo) / 2)
-			solve(lo, mid)
-			solve(mid + 1, hi)
-			merge(lo, mid, hi)
-		}
+    function solve(lo, hi) {
+      if(hi <= lo) return;
+      var mid = Math.floor((hi + lo) / 2)
+      solve(lo, mid)
+      solve(mid + 1, hi)
+      merge(lo, mid, hi)
+    }
 
-		solve(0, N - 1)
-		return inputArray;
-	}
+    solve(0, N - 1)
+    return inputArray;
+  }
 }
 module.exports = Sort;
